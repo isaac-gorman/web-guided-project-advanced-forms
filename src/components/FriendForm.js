ce94e043f61af8ddcaf60950d8b7a2e516232d5c
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-17 18:48:51 +0200

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -20,6 +20,10 @@ export default function FriendForm(props) {
 
         <div className='errors'>
           {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
+          <span>{errors.username}</span>
+          <span>{errors.email}</span>
+          <span>{errors.civil}</span>
+          <span>{errors.role}</span>
         </div>
       </div>
 
